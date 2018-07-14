b009a74be7499b23adf82dbac3ef5d8cd7cb887b
satishchandrayadav <chandrasatish2009@gmail.com>
2018-07-14 06:27:05 -0700

    renamed the program name and object name

--- scala/com/mycompany/driverCode/monthSalesDriverProgram.scala
+++ scala/com/mycompany/driverCode/monthSalesDriverProgram.scala
@@ -1,6 +1,6 @@
 package com.mycompany.drivercode
 import com.mycompany.sales
-import com.mycompany.utils.writeToCSVwdHeader
+import com.mycompany.utils.utilToWriteToCSVwdHeader
 import org.apache.spark.sql.DataFrame
 
 
@@ -9,9 +9,9 @@ object monthSalesDriverProgram  {
   def main(args: Array[String]) = {
     val salesObj = new sales
     val salesAgg = salesObj.monthlyAggregateSalesByCustomer()
-    val writemonthlyAggregateSalesByCustomer = writeToCSVwdHeader.writeToCSV(salesAgg :DataFrame ,salesObj.aggMonthlySalesPath)
+    val writemonthlyAggregateSalesByCustomer = utilToWriteToCSVwdHeader.writeToCSV(salesAgg :DataFrame ,salesObj.aggMonthlySalesPath)
     val customerSegmentbasedonTransac :DataFrame = salesObj.calculateCustomerSegmentBasedOnTransVolume()
-    val writecustomerSegment = writeToCSVwdHeader.writeToCSV(customerSegmentbasedonTransac :DataFrame ,salesObj.salesSavePath)
+    val writecustomerSegment = utilToWriteToCSVwdHeader.writeToCSV(customerSegmentbasedonTransac :DataFrame ,salesObj.salesSavePath)
 
   }
 
