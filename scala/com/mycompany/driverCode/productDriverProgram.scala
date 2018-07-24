12d6bb7d8f02cd31d446826dac9908f641ca267b
satishchandrayadav <chandrasatish2009@gmail.com>
2018-07-24 06:30:24 -0700

    added try and catch block in a file not found

--- scala/com/mycompany/driverCode/productDriverProgram.scala
+++ scala/com/mycompany/driverCode/productDriverProgram.scala
@@ -1,13 +1,52 @@
 package com.mycompany.drivercode
 
+import java.io.{FileNotFoundException, IOException}
+
 import com.mycompany.product
-import com.mycompany.utils.utilToWriteToCSVwdHeader
+import com.mycompany.utils.{utilToCountInputData, utilToWriteToCSVwdHeader}
+import org.apache.spark.sql.AnalysisException
+
+import scala.util.control.NonFatal
 
 object productDriverProgram {
   def main(args: Array[String]) = {
     val productObj = new product
-    val writeCustomerDim = utilToWriteToCSVwdHeader.writeToCSV(productObj.productSourceData ,productObj.productSavePath)
+    val fileName = productObj.productSourceDataPath
+    val programName = new Exception().getStackTrace.head.getFileName
 
+    try {
+      val inputData = s"${productObj.productSourceDataPath}"
+      println(s"Name of the insert file: ${inputData}")
+
+      val inputDataCount = utilToCountInputData.getInputDataCount(inputData)
+
+      val productSourceData = productObj.readSourceData(productObj.productSourceDataPath)
+
+      val writeProductDim = utilToWriteToCSVwdHeader.writeToCSV(productSourceData ,productObj.productSavePath)
+
+      val writeCount = productSourceData.count()
+
+
+      val status: String = "S"
+
+      productObj.getJobMetrics(programName: String, inputDataCount: String, writeCount: Long, status: String,
+        productObj.loadDate: String,
+        productObj.jobMetricsWritePath: String)
+  }
+    catch {
+
+      case e: IOException => e.printStackTrace
+      case e: IllegalArgumentException => println("illegal arg. exception");
+      case e: AnalysisException => println(s"IO exception file not found ${fileName} ")
+
+      case e: FileNotFoundException => println("Couldn't find that file.")
+      case NonFatal(_) => println("Ooops. Much better, only the non fatal exceptions end up here.")
+
+    }
+    finally {
+      println("Running finally block")
+
+    }
 
   }
 }
