16d301e5f49f04dfa534b1da066ab85a9e506bd7
satishchandrayadav <chandrasatish2009@gmail.com>
2018-10-14 04:14:24 -0700

    parameterized batch file

--- batchScripts/product_dim.sh
+++ batchScripts/product_dim.sh
@@ -1,2 +1,40 @@
 #!/bin/bash
-spark-submit --class com.mycompany.drivercode.productDriverProgram /customerSegment/target/scala-2.11/-customersegment_2.11-0.1.jar
+
+#location of config file
+
+config_loc="/customerSegment/src/main/scala/com/mycompany/config/customerConfig.json"
+
+#get list of tables
+
+        table_list=$(cat /customerSegment/src/main/scala/com/mycompany/config/customerConfig.json | jq  '.sales_dev_cloud.tables | keys[]')
+
+
+        #get S3 path location & file existennce
+
+        file_path=$(for i in $table_list; do cat $config_loc | jq -r ".sales_dev_cloud.tables.$i.table_location"; done;)
+
+
+        file_missing_counter=0
+
+        for i in $file_path; do exists=$(aws s3 ls $i);
+        if [[ -z "$exists" ]];
+        then echo "$i bucket does not exit";
+           file_missing_counter=$((file_missing_counter+1));
+           else
+                echo " $i bucket exit";
+       fi ;
+       done
+
+
+#exit in case file not found.
+
+      if [[ $file_missing_counter -gt 0 ]]; then
+            echo "file not found" ; exit 1;
+      fi
+
+
+#submit spark program
+
+      spark-submit --class com.mycompany.drivercode.productDriverProgram /customerSegment/target/scala-2.11/-customersegment_2.11-0.1.jar
+
+
